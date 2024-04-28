fb57a791d64fdfeddfd67efbc950d8e0653a3168
Scalas <scala0114@gmail.com>
2024-04-28 20:37:53 +0900

    feat: support multi cs-code

--- background.js
+++ background.js
@@ -16,28 +16,14 @@ chrome.contextMenus.onClicked.addListener(async (info) => {
     return;
   }
   const coupons = info.selectionText.replace(/\([^\)]*\)/gi,'').trim().split(/\s+/);
-  const csCode = (await chrome.storage.sync.get('csCode'))['csCode'];
+  const csCodeList = (await chrome.storage.sync.get('csCode'))['csCode'];
   const pageKey = await fetch('https://coupon.withhive.com/1614')
       .then(async (response) => {
         const text = await response.text();
         return text.match(pattern)[1];
       });
-  const ADDITIONAL_INFO = await fetch('https://coupon.withhive.com/tp/coupon/server_list', {
-    method: 'POST',
-    headers: {
-      'Page-Key': pageKey
-    },
-    body: JSON.stringify({
-      language: 'ko',
-      server: '1614|KR|KR',
-      cs_code: csCode,
-    })
-  }).then(async (response) => {
-    return (await response.json())['serverList']['0']['additionalinfo'];
-  });
-  //console.log(coupons, csCode);
-  const promises = coupons.map(async (coupon) => {
-    await fetch('https://coupon.withhive.com/tp/coupon/use', {
+  for (const csCode of csCodeList) {
+    const ADDITIONAL_INFO = await fetch('https://coupon.withhive.com/tp/coupon/server_list', {
       method: 'POST',
       headers: {
         'Page-Key': pageKey
@@ -46,21 +32,36 @@ chrome.contextMenus.onClicked.addListener(async (info) => {
         language: 'ko',
         server: '1614|KR|KR',
         cs_code: csCode,
-        coupon: coupon,
-        additional_info:ADDITIONAL_INFO
       })
     }).then(async (response) => {
-      const result = (await response.json())['msg'];
-      //console.log(`coupon: ${coupon}, result: ${result}`);
-	  chrome.notifications.create({
-      type: 'basic',
-      iconUrl: '/images/alarm.jpeg',
-      title: `스타시드 쿠폰 사용 - CS코드 : ${csCode}`,
-      message: `${coupon} : ${result}`,
-      priority: 2
-      });
-      return result;
+      return (await response.json())['serverList']['0']['additionalinfo'];
     });
-  });
-  await Promise.all(promises);
+    //console.log(coupons, csCode);
+    const promises = coupons.map(async (coupon) => {
+      await fetch('https://coupon.withhive.com/tp/coupon/use', {
+        method: 'POST',
+        headers: {
+          'Page-Key': pageKey
+        },
+        body: JSON.stringify({
+          language: 'ko',
+          server: '1614|KR|KR',
+          cs_code: csCode,
+          coupon: coupon,
+          additional_info:ADDITIONAL_INFO
+        })
+      }).then(async (response) => {
+        const result = (await response.json())['msg'];
+        chrome.notifications.create({
+          type: 'basic',
+          iconUrl: '/images/alarm.jpeg',
+          title: `스타시드 쿠폰 사용 - CS코드 : ${csCode}`,
+          message: `${coupon} : ${result}`,
+          priority: 2
+        });
+        return result;
+      });
+    });
+    await Promise.all(promises);
+  }
 });
--- popup.js
+++ popup.js
@@ -1,15 +1,33 @@
 const csCodeInput = document.querySelector('#csCodeInput');
 const saveButton = document.querySelector('#csCodeSaveButton');
+const savedCsCode = document.querySelector("#savedCsCode");
 
 window.addEventListener('load', async () => {
-  const current = (await chrome.storage.sync.get('csCode'))['csCode'];
-  if (current != null) {
-    csCodeInput.value = current;
-  }
+  const current = (await chrome.storage.sync.get('csCode'))['csCode'] ?? [];
+  current.map((csCode) => {
+    const div = document.createElement('div');
+    div.className = 'csCodeListItem';
+    const span = document.createElement('span');
+    span.textContent = csCode;
+    const button = document.createElement('input');
+    button.type = 'button';
+    button.value = '삭제';
+    button.addEventListener('click', async (e) => {
+      const current = (await chrome.storage.sync.get('csCode'))['csCode'] ?? [];
+      const next = current.filter((code) => code !== csCode);
+      chrome.storage.sync.set({ 'csCode': next });
+      window.close();
+    })
+    div.appendChild(span);
+    div.appendChild(button);
+    savedCsCode.appendChild(div);
+  });
 });
 
 saveButton.addEventListener('click', async () => {
+  const current = (await chrome.storage.sync.get('csCode'))['csCode'] ?? [];
   const csCode = csCodeInput.value;
-  chrome.storage.sync.set({ 'csCode': csCode });
+  const next = csCode in current ? current : [...current, csCode];
+  chrome.storage.sync.set({ 'csCode': next });
   window.close();
 });
